9ed2bef853612fefcd5d328633ca7e9b5fe96f77
Andy Seaborne <andy.seaborne@gmail.com>
2012-01-06 16:58:53 +0000

    Update to Jena 2.7.0 and Sesame 2.6.2

--- src/org/openjena/jenasesame/impl/QueryIteratorSesame.java
+++ src/org/openjena/jenasesame/impl/QueryIteratorSesame.java
@@ -88,7 +88,7 @@ public class QueryIteratorSesame extends QueryIteratorBase
 
 /*
  * (c) Copyright 2009 Talis Information Ltd.
- * (c) Copyright 2010 Epimorphics Ltd.
+ * (c) Copyright 2010-2012 Epimorphics Ltd.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
