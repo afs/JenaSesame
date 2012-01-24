b1981ef78a928facb0fe18d7d64b7decefa2b338
Peter Ansell <p_ansell@yahoo.com>
2012-01-24 13:55:57 +1000

    cleanup Convert

--- jena-sesame-common/src/main/java/org/openjena/jenasesame/util/Convert.java
+++ jena-sesame-common/src/main/java/org/openjena/jenasesame/util/Convert.java
@@ -1,59 +1,46 @@
 /*
- * (c) Copyright 2009 Talis Information Ltd.
- * All rights reserved.
- * [See end of file]
+ * (c) Copyright 2009 Talis Information Ltd. All rights reserved. [See end of file]
  */
 
 package org.openjena.jenasesame.util;
 
-import org.openrdf.model.BNode ;
-import org.openrdf.model.Literal ;
-import org.openrdf.model.Resource ;
-import org.openrdf.model.Statement ;
-import org.openrdf.model.URI ;
-import org.openrdf.model.Value ;
-import org.openrdf.model.ValueFactory ;
+import org.openrdf.model.BNode;
+import org.openrdf.model.Literal;
+import org.openrdf.model.Resource;
+import org.openrdf.model.Statement;
+import org.openrdf.model.URI;
+import org.openrdf.model.Value;
+import org.openrdf.model.ValueFactory;
 
-import com.hp.hpl.jena.graph.Node ;
-import com.hp.hpl.jena.graph.Triple ;
-import com.hp.hpl.jena.rdf.model.AnonId ;
+import com.hp.hpl.jena.graph.Node;
+import com.hp.hpl.jena.graph.Triple;
+import com.hp.hpl.jena.rdf.model.AnonId;
 import com.hp.hpl.jena.rdf.model.Model;
-import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.rdf.model.Property;
 import com.hp.hpl.jena.rdf.model.RDFNode;
 
 public class Convert
 {
-    public static com.hp.hpl.jena.rdf.model.Resource resourceToResource(Model nextModel, Resource resource)
+    public static Node bnodeToNode(final BNode value)
     {
-        if(resource instanceof URI)
-        {
-            return nextModel.createResource(resource.stringValue());
-        }
-        else
-        {
-            return nextModel.createResource(new AnonId(resource.stringValue()));
-        }
+        return Node.createAnon(new AnonId(value.getID()));
     }
     
-    public static com.hp.hpl.jena.rdf.model.Property uriToProperty(Model nextModel, URI resource)
+    public static Node literalToNode(final Literal value)
     {
-        return nextModel.createProperty(resource.stringValue());
+        if(value.getLanguage() != null)
+        {
+            return Node.createLiteral(value.getLabel(), value.getLanguage(), false);
+        }
+        if(value.getDatatype() != null)
+        {
+            return Node.createLiteral(value.getLabel(), null, Node.getType(value.getDatatype().stringValue()));
+        }
+        // Plain literal
+        return Node.createLiteral(value.getLabel());
     }
     
-    public static RDFNode valueToRDFNode(Model nextModel, Value value)
-    {
-        if(value instanceof Resource)
-        {
-            return resourceToResource(nextModel, (Resource)value);
-        }
-        else
-        {
-            return literalToRDFNode(nextModel, (Literal)value);
-        }
-    }
-    
-    public static RDFNode literalToRDFNode(Model nextModel, Literal value)
+    public static RDFNode literalToRDFNode(final Model nextModel, final Literal value)
     {
         if(value.getDatatype() != null)
         {
@@ -68,137 +55,164 @@ public class Convert
             return nextModel.createLiteral(value.stringValue());
         }
     }
-
-    public static Node valueToNode(Value value)
+    
+    public static BNode nodeBlankToValue(final ValueFactory factory, final Node node)
     {
-        if ( value instanceof Literal )
-            return literalToNode((Literal)value) ; 
-        if ( value instanceof URI )
-            return uriToNode((URI)value) ; 
-        if ( value instanceof BNode )
-            return bnodeToNode((BNode)value) ; 
-        throw new IllegalArgumentException("Not a concrete value") ;
+        return factory.createBNode(node.getBlankNodeLabel());
     }
     
-    public static Node resourceToNode(Resource resource)
+    public static Value nodeLiteralToValue(final ValueFactory factory, final Node node)
     {
-        return valueToNode(resource);
+        if(node.getLiteralDatatype() != null)
+        {
+            final URI x = factory.createURI(node.getLiteralDatatypeURI());
+            return factory.createLiteral(node.getLiteralLexicalForm(), x);
+        }
+        if(!node.getLiteralLanguage().equals(""))
+        {
+            return factory.createLiteral(node.getLiteralLexicalForm(), node.getLiteralLanguage());
+        }
+        
+        return factory.createLiteral(node.getLiteralLexicalForm());
     }
     
-    public static Node bnodeToNode(BNode value)
+    public static Value nodeToValue(final ValueFactory factory, final Node node)
     {
-        return Node.createAnon(new AnonId(value.getID())) ;
-    }
-
-    public static Node uriToNode(URI value)
-    {
-        return Node.createURI(value.stringValue()) ;
-    }
-
-    public static Node literalToNode(Literal value)
-    {
-        if ( value.getLanguage() != null )
-            return Node.createLiteral(value.getLabel(), value.getLanguage(), false) ;
-        if ( value.getDatatype() != null )
-            return Node.createLiteral(value.getLabel(),null, Node.getType(value.getDatatype().stringValue())) ;
-        // Plain literal
-        return Node.createLiteral(value.getLabel()) ;
-    }
-
-    public static Triple statementToTriple(Statement stmt)
-    {
-        Node s = Convert.resourceToNode(stmt.getSubject()) ;
-        Node p = Convert.uriToNode(stmt.getPredicate()) ;
-        Node o = Convert.valueToNode(stmt.getObject()) ;            
-        return new Triple(s,p,o) ;
+        if(node.isLiteral())
+        {
+            return Convert.nodeLiteralToValue(factory, node);
+        }
+        if(node.isURI())
+        {
+            return Convert.nodeURIToValue(factory, node);
+        }
+        if(node.isBlank())
+        {
+            return Convert.nodeBlankToValue(factory, node);
+        }
+        throw new IllegalArgumentException("Not a concrete node");
     }
     
-    public static com.hp.hpl.jena.rdf.model.Statement statementToJenaStatement(Model nextModel, Statement stmt)
+    public static Resource nodeToValueResource(final ValueFactory factory, final Node node)
     {
-        com.hp.hpl.jena.rdf.model.Resource s = Convert.resourceToResource(nextModel, stmt.getSubject());
-        Property p = Convert.uriToProperty(nextModel, stmt.getPredicate()) ;
-        RDFNode o = Convert.valueToRDFNode(nextModel, stmt.getObject()) ;            
+        if(node.isURI())
+        {
+            return Convert.nodeURIToValue(factory, node);
+        }
+        if(node.isBlank())
+        {
+            return Convert.nodeBlankToValue(factory, node);
+        }
+        throw new IllegalArgumentException("Neither a URI nor a blank node");
+    }
+    
+    public static URI nodeURIToValue(final ValueFactory factory, final Node node)
+    {
+        return factory.createURI(node.getURI());
+    }
+    
+    public static Node resourceToNode(final Resource resource)
+    {
+        return Convert.valueToNode(resource);
+    }
+    
+    public static com.hp.hpl.jena.rdf.model.Resource resourceToResource(final Model nextModel, final Resource resource)
+    {
+        if(resource instanceof URI)
+        {
+            return nextModel.createResource(resource.stringValue());
+        }
+        else
+        {
+            return nextModel.createResource(new AnonId(resource.stringValue()));
+        }
+    }
+    
+    public static com.hp.hpl.jena.rdf.model.Statement statementToJenaStatement(final Model nextModel,
+            final Statement stmt)
+    {
+        final com.hp.hpl.jena.rdf.model.Resource s = Convert.resourceToResource(nextModel, stmt.getSubject());
+        final Property p = Convert.uriToProperty(nextModel, stmt.getPredicate());
+        final RDFNode o = Convert.valueToRDFNode(nextModel, stmt.getObject());
         
         return nextModel.createStatement(s, p, o);
     }
     
-    public static Statement tripleToStatement(ValueFactory factory, Triple triple)
+    public static Triple statementToTriple(final Statement stmt)
     {
-        return factory.createStatement(nodeToValueResource(factory, triple.getSubject()), nodeURIToValue(factory, triple.getPredicate()), nodeToValue(factory, triple.getObject()));
+        final Node s = Convert.resourceToNode(stmt.getSubject());
+        final Node p = Convert.uriToNode(stmt.getPredicate());
+        final Node o = Convert.valueToNode(stmt.getObject());
+        return new Triple(s, p, o);
     }
     
     // ----
     // Problems with the ValueFactory
     
-    public static Value nodeToValue(ValueFactory factory, Node node)
+    public static Statement tripleToStatement(final ValueFactory factory, final Triple triple)
     {
-        if ( node.isLiteral() )
-            return nodeLiteralToValue(factory, node) ;
-        if ( node.isURI() )
-            return nodeURIToValue(factory, node) ;
-        if ( node.isBlank() )
-            return nodeBlankToValue(factory, node) ;
-        throw new IllegalArgumentException("Not a concrete node") ;
+        return factory.createStatement(Convert.nodeToValueResource(factory, triple.getSubject()),
+                Convert.nodeURIToValue(factory, triple.getPredicate()),
+                Convert.nodeToValue(factory, triple.getObject()));
     }
     
-    public static Resource nodeToValueResource(ValueFactory factory, Node node)
+    public static Node uriToNode(final URI value)
     {
-        if ( node.isURI() )
-            return nodeURIToValue(factory, node) ;
-        if ( node.isBlank() )
-            return nodeBlankToValue(factory, node) ;
-        throw new IllegalArgumentException("Neither a URI nor a blank node") ;
+        return Node.createURI(value.stringValue());
     }
-
-    public static BNode nodeBlankToValue(ValueFactory factory, Node node)
+    
+    public static com.hp.hpl.jena.rdf.model.Property uriToProperty(final Model nextModel, final URI resource)
     {
-        return factory.createBNode(node.getBlankNodeLabel()) ;
+        return nextModel.createProperty(resource.stringValue());
     }
-
-    public static URI nodeURIToValue(ValueFactory factory, Node node)
+    
+    public static Node valueToNode(final Value value)
     {
-        return factory.createURI(node.getURI()) ;
-    }
-
-    public static Value nodeLiteralToValue(ValueFactory factory, Node node)
-    {
-        if ( node.getLiteralDatatype() != null )
+        if(value instanceof Literal)
         {
-            URI x = factory.createURI(node.getLiteralDatatypeURI()) ;
-            return factory.createLiteral(node.getLiteralLexicalForm(), x) ;
+            return Convert.literalToNode((Literal)value);
         }
-        if ( ! node.getLiteralLanguage().equals("") )
+        if(value instanceof URI)
         {
-            return factory.createLiteral(node.getLiteralLexicalForm(), node.getLiteralLanguage()) ;
+            return Convert.uriToNode((URI)value);
         }
-        
-        return factory.createLiteral(node.getLiteralLexicalForm()) ;
-    }        
+        if(value instanceof BNode)
+        {
+            return Convert.bnodeToNode((BNode)value);
+        }
+        throw new IllegalArgumentException("Not a concrete value");
+    }
+    
+    public static RDFNode valueToRDFNode(final Model nextModel, final Value value)
+    {
+        if(value instanceof Resource)
+        {
+            return Convert.resourceToResource(nextModel, (Resource)value);
+        }
+        else
+        {
+            return Convert.literalToRDFNode(nextModel, (Literal)value);
+        }
+    }
 }
 
 /*
- * (c) Copyright 2009 Talis Information Ltd.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
- * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
- * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * (c) Copyright 2009 Talis Information Ltd. All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification, are permitted
+ * provided that the following conditions are met: 1. Redistributions of source code must retain the
+ * above copyright notice, this list of conditions and the following disclaimer. 2. Redistributions
+ * in binary form must reproduce the above copyright notice, this list of conditions and the
+ * following disclaimer in the documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products derived from this
+ * software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
